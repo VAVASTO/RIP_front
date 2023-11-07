90cc16d5a363a40d826b7b6a68ebdeb45f9864a2
vladimir <vladimir.polotsk@gmail.com>
2023-11-08 00:38:07 +0300

    test5

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,18 @@ import BouquetDetailPage from './BouquetDetail';  // Import your BouquetDetail c
 //import { useNavigate } from 'react-router-dom';
 // import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path= '/bouquetss' element={<BouquetsPage />}/>
           <Route path= '/' element={<h1>Это наша страница с чем-то новеньким</h1>}/>
           <Route path= '/bouquetss/:id/' element={<BouquetDetailPage />} />
 
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
   )
 }
 
