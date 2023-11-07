4882d72406ae55cc4cc5e8e57e3de049e0889707
vladimir <vladimir.polotsk@gmail.com>
2023-11-08 00:30:18 +0300

    test5

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
@@ -9,3 +10,34 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 )
 
 
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import BouquetsPage from './Bouquets';
+// import BouquetDetailPage from './BouquetDetail';  // Import your BouquetDetail component
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <h1>Это наша стартовая страница</h1>,
+//   },
+//   {
+//     path: '/RIP_front/',
+//     element: <h1>Это наша страница с чем-то новеньким</h1>,
+//   },
+//   {
+//     path: '/bouquetss',
+//     element: <BouquetsPage />,
+//   },
+//   {
+//     path: '/bouquetss/:id/',
+//     element: <BouquetDetailPage />,
+//   },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <hr />
+//     <RouterProvider router={router} />
+//   </React.StrictMode>,
+// );
