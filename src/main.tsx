4369a54abb5bc90cf78c83ce3164421c3e60d782
vladimir <vladimir.polotsk@gmail.com>
2023-11-08 00:13:59 +0300

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import BouquetsPage from './Bouquets';
-import BouquetDetailPage from './BouquetDetail';  // Import your BouquetDetail component
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <h1>Это наша стартовая страница</h1>,
-  },
-  {
-    path: '/RIP_front/',
-    element: <h1>Это наша страница с чем-то новеньким</h1>,
-  },
-  {
-    path: '/bouquetss',
-    element: <BouquetsPage />,
-  },
-  {
-    path: '/bouquetss/:id/',
-    element: <BouquetDetailPage />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <hr />
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
-);
+)
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import BouquetsPage from './Bouquets';
+// import BouquetDetailPage from './BouquetDetail';  // Import your BouquetDetail component
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <h1>Это наша стартовая страница</h1>,
+//   },
+//   {
+//     path: '/RIP_front/',
+//     element: <h1>Это наша страница с чем-то новеньким</h1>,
+//   },
+//   {
+//     path: '/bouquetss',
+//     element: <BouquetsPage />,
+//   },
+//   {
+//     path: '/bouquetss/:id/',
+//     element: <BouquetDetailPage />,
+//   },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <hr />
+//     <RouterProvider router={router} />
+//   </React.StrictMode>,
+// );
